f3fed878a7b143e29d7778d0a531c56322f00385
Youssef-Adly <youssefadly404@gmail.com>
2024-09-09 18:58:49 +0300

    chart and table and tooltip

--- src/app/Components/chart/chart.component.ts
+++ src/app/Components/chart/chart.component.ts
@@ -1,12 +1,90 @@
 import { Component } from '@angular/core';
+import * as echarts from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [],
+  imports: [NgxEchartsDirective],
   templateUrl: './chart.component.html',
-  styleUrl: './chart.component.css'
+  styleUrl: './chart.component.css',
+  providers: [provideEcharts()],
 })
 export class ChartComponent {
+  gradientColor = ['#8A74F9', '#8a74f943', '#8a74f900'];
 
+  options: echarts.EChartsOption = {
+    title: {
+      text: 'إحصائيات إستخدام القسيمة',
+      left: 'right',
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      axisPointer: {
+        value: 'اغسطس',
+        type: 'line',
+        snap: true,
+        lineStyle: {
+          color: '#7581BD',
+          width: 2,
+        },
+        label: {
+          show: true,
+          formatter: function (params: any) {
+            return params.value;
+          },
+          backgroundColor: '#7581BD',
+        },
+        handle: {
+          show: true,
+          color: '#7581BD',
+        },
+      },
+      data: [
+        'ديسمبر',
+        'نوفمبر',
+        'اكتوبر',
+        'سبتمبر',
+        'اغسطس',
+        'يونيو',
+        'مايو',
+        'ابريل',
+        'مارس',
+        'فبراير',
+        'يناير',
+      ],
+    },
+    yAxis: {
+      type: 'value',
+      show: false,
+    },
+    series: [
+      {
+        data: [820, 820, 820, 820, 820, 820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true,
+        lineStyle: {
+          width: 3,
+          type: 'solid',
+        },
+        areaStyle: {},
+      },
+    ],
+    markLine: {},
+    areaStyle: {
+      color: {
+        type: 'linear',
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        colorStops: [
+          { offset: 0, color: this.gradientColor[0] },
+          { offset: 0.5, color: this.gradientColor[1] },
+          { offset: 1, color: this.gradientColor[2] },
+        ],
+      },
+    },
+  };
 }
--- src/app/Components/table/table.component.ts
+++ src/app/Components/table/table.component.ts
@@ -1,12 +1,242 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { TooltipModule } from 'primeng/tooltip';
+import { FileSaverModule, FileSaverService } from 'ngx-filesaver';
+import FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [
+    TableModule,
+    CommonModule,
+    AvatarModule,
+    BadgeModule,
+    FileSaverModule,
+    TooltipModule,
+  ],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css'
+  styleUrl: './table.component.css',
 })
 export class TableComponent {
+  customers = [
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 10,
+      numOfPeople: 1,
+    },
+    {
+      id: 2,
+      name: 'احمد سيد',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 20,
+      numOfPeople: 1,
+    },
+    {
+      id: 3,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 40,
+      numOfPeople: 1,
+    },
+    {
+      id: 4,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 100,
+      numOfPeople: 1,
+    },
+    {
+      id: 5,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 80,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 180,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 40,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 100,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 80,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 180,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 10,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 20,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 40,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 100,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 80,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 180,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 40,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 100,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 80,
+      numOfPeople: 1,
+    },
+    {
+      id: 1,
+      name: 'احمد محمود',
+      ticketNumber: 'C-107',
+      phone: '01286546',
+      numofBuys: 'مره واحده',
+      address: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة',
+      price: 180,
+      numOfPeople: 1,
+    },
+  ];
 
+  downloadFile() {
+    const fileUrl =
+      'https://drive.google.com/file/d/1Kdryi45jLttwmbWhQtiPtbjOz_Ja_y3E/view';
+
+    fetch(fileUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const blobUrl = URL.createObjectURL(blob);
+        FileSaver.saveAs(blobUrl, 'Youssef-Adly CV.pdf');
+      })
+      .catch((error) => {
+        console.error('Error downloading file:', error);
+      });
+  }
 }
