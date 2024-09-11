0fc5f8ab62b79d36f6a61220c3379643500ef2dd
Youssef-Adly <youssefadly404@gmail.com>
2024-09-11 16:59:26 +0300

    add inpputs before table

--- src/app/Pages/landing-page/landing-page.component.ts
+++ src/app/Pages/landing-page/landing-page.component.ts
@@ -2,16 +2,24 @@ import { Component } from '@angular/core';
 import { ChartComponent } from '../../Components/chart/chart.component';
 import { TableComponent } from '../../Components/table/table.component';
 import { CardComponent } from '../../Components/card/card.component';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputSwitchModule } from 'primeng/inputswitch';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ChartComponent, TableComponent, CardComponent],
+  imports: [
+    ChartComponent,
+    TableComponent,
+    CardComponent,
+    IconFieldModule,
+    InputTextModule,
+    InputIconModule,
+    InputSwitchModule,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
-export class LandingPageComponent {
-  private data = {
-    
-  }
-}
+export class LandingPageComponent {}
